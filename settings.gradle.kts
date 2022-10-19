11b2a4285b130fc2963d50d907c70595368c0ae8
antoniolg <antonio@devexperto.com>
2022-10-19 13:00:37 +0200

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.devexperto.mynotes-server"
---
+++ src/main/kotlin/com/devexperto/Application.kt
@@ -0,0 +1,11 @@
+package com.devexperto
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.devexperto.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
