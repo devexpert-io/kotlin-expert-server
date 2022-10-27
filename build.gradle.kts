5440c8c1130666e859f42b80207d7b08f6409ee2
antoniolg <antonio@devexperto.com>
2022-10-27 09:52:30 +0200

    Basic routing sample

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/com/devexperto/plugins/Routing.kt
+++ src/main/kotlin/com/devexperto/plugins/Routing.kt
@@ -1,10 +1,11 @@
 package com.devexperto.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
 
@@ -13,7 +14,31 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+        get("html") {
+            call.respondHtml(HttpStatusCode.OK) {
+                head {
+                    title { +"Hello Ktor" }
+                }
+                body {
+                    h1 { +"This is an h1" }
+                    p { +"This is a paragraph" }
+                    div {
+                        a(href = "html/clicked") { +"Click Me" }
+                    }
+                }
+            }
+        }
+
+        get("html/clicked") {
+            call.respondHtml {
+                head {
+                    title { +"Clicked" }
+                }
+                body {
+                    h1 { +"Button clicked" }
+                }
+            }
+        }
     }
-    routing {
-    }
+
 }
