cec611faa32656131776a03ed94bafa114a93500
antoniolg <antonio@devexperto.com>
2023-03-23 11:31:28 +0100

    Configure CORS plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("io.ktor:ktor-server-html-builder:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-cors:$ktor_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
